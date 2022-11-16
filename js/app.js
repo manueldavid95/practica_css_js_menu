eee6e3d3e6e48af1b75148fabaf74d3cd979fff0
manueldavid95 <manueldavidrangel23@gmail.com>
2022-11-16 11:27:21 -0400

    menu movil finalizado

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let iconsNav = document.querySelector('.icons-nav'); 
 let menuOpen = document.querySelector('.open');
 let menuClose = document.querySelector('.close');
+let listaNav = document.querySelector('.lista-nav');
 
 console.log(iconsNav);
 console.log(menuOpen);
@@ -8,8 +9,11 @@ console.log(menuClose);
 
 menuOpen.addEventListener('click', function() {
     iconsNav.classList.add('active')
+    listaNav.classList.add('menu-active')
 })
 
 menuClose.addEventListener('click', function() {
     iconsNav.classList.remove('active')
+    listaNav.classList.remove('menu-active')
 })
+
